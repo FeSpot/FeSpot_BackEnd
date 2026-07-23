1ec12002a8ffc18342bd6d98ca449b2b5d163602
naeunmin <babaleo115@gmail.com>
2026-07-23 15:22:32 +0900

    feat : 축제 조회 예외 처리 작성 - 한국관광공사 API 호출 실패를 공통 CustomException으로 처리하는 오류 코드를 추가

---
+++ src/main/java/com/api/fespot/domain/festival/exception/FestivalErrorResponseCode.java
@@ -0,0 +1,15 @@
+package com.api.fespot.domain.festival.exception;
+
+import com.api.fespot.global.response.code.BaseResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum FestivalErrorResponseCode implements BaseResponseCode {
+    KTO_API_ERROR_502("FESTIVAL_KTO_API_ERROR_502", 502, "한국관광공사 OpenAPI에서 축제 정보를 불러오지 못했습니다.");
+
+    private final String code;
+    private final int httpStatus;
+    private final String message;
+}
