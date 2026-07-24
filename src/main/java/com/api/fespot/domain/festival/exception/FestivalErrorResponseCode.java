acf689105491e45d3443bf7e624120ca86169c5e
naeunmin <babaleo115@gmail.com>
2026-07-25 00:55:05 +0900

    feat : 축제 정보 에러코드 추가 - FESTIVAL_NOT_FOUND_404 추가

--- src/main/java/com/api/fespot/domain/festival/exception/FestivalErrorResponseCode.java
+++ src/main/java/com/api/fespot/domain/festival/exception/FestivalErrorResponseCode.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum FestivalErrorResponseCode implements BaseResponseCode {
+    FESTIVAL_NOT_FOUND_404("FESTIVAL_NOT_FOUND_404", 404, "축제 정보를 찾을 수 없습니다."),
     KTO_API_ERROR_502("FESTIVAL_KTO_API_ERROR_502", 502, "한국관광공사 OpenAPI에서 축제 정보를 불러오지 못했습니다.");
 
     private final String code;
